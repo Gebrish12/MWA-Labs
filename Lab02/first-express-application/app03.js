df86c089a38538c344baa6f833e7727eaecba61f
Gebrish12 <87670636+Gebrish12@users.noreply.github.com>
2021-09-28 18:21:47 -0500

    Update app03.js

--- Lab02/first-express-application/app03.js
+++ Lab02/first-express-application/app03.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.set("port",3000);
+app.set("port",5353);
 
 app.use(express.static(path.join(__dirname,"public")))
 
