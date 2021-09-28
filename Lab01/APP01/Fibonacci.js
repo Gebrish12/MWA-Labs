3727166c6f039496e8e3a7c678c5241b81132103
Gebrish12 <87670636+Gebrish12@users.noreply.github.com>
2021-09-27 20:56:34 -0500

    fibonacci

---
+++ Lab01/APP01/Fibonacci.js
@@ -0,0 +1,14 @@
+module.exports = function(num) {
+  var number =num;
+ if(num<0){
+    number = num*-1;
+ }
+  fib =[];
+  fib[0]=0;
+  fib[1]=1;
+  for (var i=2;i<=number;i++){
+    fib[i]=fib[i-2]+fib[i-1];
+  }
+ return fib;
+
+}
---
+++ Lab01/APP01/app.js
@@ -0,0 +1,5 @@
+
+const fibonacci = require('./Fibonacci');
+
+console.log(fibonacci(30));
+console.log(fibonacci(-15));
